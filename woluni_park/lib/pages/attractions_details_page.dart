bc4ad8f817b5874c5a703b8590be9c788092b2d3
Niek Geerligs <geerligsniek@gmail.com>
2026-03-19 09:05:03 +0100

    added assets

---
+++ woluni_park/lib/pages/attractions_details_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AttractionsDetailsPage extends StatefulWidget {
+  const AttractionsDetailsPage({super.key});
+
+  @override
+  State<AttractionsDetailsPage> createState() => _AttractionsDetailsPageState();
+}
+
+class _AttractionsDetailsPageState extends State<AttractionsDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ woluni_park/lib/pages/park_map_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ParkMapPage extends StatefulWidget {
+  const ParkMapPage({super.key});
+
+  @override
+  State<ParkMapPage> createState() => _ParkMapPageState();
+}
+
+class _ParkMapPageState extends State<ParkMapPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
